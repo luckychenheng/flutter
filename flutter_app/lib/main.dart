88b926b051645cecd9803b3dace9b44df025254a
lucky <seemac@seedeMacBook-Pro.local>
2018-12-27 15:06:15 +0800

    Commit

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/view_controller/app.dart';
+
+void main() => runApp(LuckyApp());
---
+++ flutter_app/lib/view_controller/app.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_app/view_controller/movie.dart';
+import 'package:flutter_app/view_controller/book.dart';
+import 'package:flutter_app/view_controller/music.dart';
+
+class LuckyApp extends StatefulWidget {
+
+  @override
+  LuckyAppState createState() => new LuckyAppState();
+}
+
+class LuckyAppState extends State<LuckyApp>
+    with SingleTickerProviderStateMixin {
+  TabController controller;
+
+  @override
+  void initState() {
+    controller = new TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        body: new TabBarView(
+          controller: controller,
+          children: [
+            new Movie(),
+            new Book(),
+            new Music(),
+          ],
+        ),
+        bottomNavigationBar: new Material(
+          color: Colors.white,
+          child: new TabBar(
+            controller: controller,
+            labelColor: Colors.red,
+            unselectedLabelColor: Colors.black,
+            indicatorWeight: 1,
+            indicatorColor: Colors.white,
+            tabs: [
+              new Tab(
+                text: "首页",
+                icon: new Icon(
+                  Icons.movie,
+                ),
+              ),
+              new Tab(
+                text: "赚赚",
+                icon: new Icon(
+                  Icons.book,
+                ),
+              ),
+              new Tab(
+                text: "我的",
+                icon: new Icon(
+                  Icons.access_alarm,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
